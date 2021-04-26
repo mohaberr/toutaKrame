2542fcb6ebb0f1091c0ab45d71a03373f5b0e5e4
mohaberr <mohammedberryah@gmail.com>
2021-04-26 12:04:45 +0200

    class et manager

--- PHP/MODEL/IngredientsManager.Class.php
+++ PHP/MODEL/IngredientsManager.Class.php
@@ -3,22 +3,18 @@ class IngredientsManager{
 //******************************** select ****************************
 public static function add(Ingredients $cl){
 $db=DbConnect::getDb();
-$requete = $db->prepare("INSERT INTO Ingredients(libelleIngredient,quantite,idRecette,idUnite)VALUES (:libelleIngredient,:quantite,:idRecette,:idUnite)");
+$requete = $db->prepare("INSERT INTO Ingredients(libelleIngredient,idUnite)VALUES (:libelleIngredient,:idUnite)");
 $requete->bindValue(':libelleIngredient',$cl->getLibelleIngredient());
-$requete->bindValue(':quantite',$cl->getQuantite());
-$requete->bindValue(':idRecette',$cl->getIdRecette());
 $requete->bindValue(':idUnite',$cl->getIdUnite());
 $res = $requete->execute();
 }
 //******************************** UPDATE ****************************
 public static function UPDATE(Ingredients $cl){
 $db=DbConnect::getDb();
-$requete = $db->prepare("UPDATE Ingredients SET idIngredient=:idIngredient,libelleIngredient=:libelleIngredient,quantite=:quantite,idRecette=:idRecette,idUnite=:idUnite WHERE idIngredient=:idIngredient");
+$requete = $db->prepare("UPDATE Ingredients SET idIngredient=:idIngredient,libelleIngredient=:libelleIngredient,idUnite=:idUnite WHERE idIngredient=:idIngredient");
 
 $requete->bindValue(':idIngredient',$cl->getIdIngredient());
 $requete->bindValue(':libelleIngredient',$cl->getLibelleIngredient());
-$requete->bindValue(':quantite',$cl->getQuantite());
-$requete->bindValue(':idRecette',$cl->getIdRecette());
 $requete->bindValue(':idUnite',$cl->getIdUnite());
 $res = $requete->execute();
 }
