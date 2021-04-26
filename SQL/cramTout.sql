96e815a3b8240c6de83c83bf160fb1c4be9779fa
nilsKolliker <nils.kolliker@gmail.com>
2021-04-26 09:47:35 +0200

    minimum requis

--- SQL/cramTout.sql
+++ SQL/cramTout.sql
@@ -1,7 +1,7 @@
 #------------------------------------------------------------
 #        Script MySQL.
 #------------------------------------------------------------
-DROP DATABASE toutaKrame;
+DROP DATABASE IF EXISTS toutaKrame;
 CREATE DATABASE IF NOT EXISTS toutaKrame ;
 USE toutaKrame;
 
---
+++ index.php
@@ -0,0 +1,39 @@
+<?PHP
+
+include "./PHP/Outils.php";
+spl_autoload_register("ChargerClasse");
+
+//on active la connexion à la base de données
+Parametre::init();
+DbConnect::init();
+
+session_start(); // initialise la variable de Session
+
+$routes=[
+    "default"=>["PHP/VIEW/","ListeRecettes","Acceuil"]
+];
+
+if (isset($_GET["codePage"]))
+{
+
+    $codePage = $_GET["codePage"];
+
+    //Si cette route existe dans le tableau des routes
+    if (isset($routes[$codePage]))
+    {
+        //Afficher la page correspondante
+        AfficherPage($routes[$codePage]);
+    }
+    else
+    {
+        //Sinon afficher la page par defaut
+        AfficherPage($routes["default"]);
+    }
+
+}
+else
+{
+    //Sinon afficher la page par defaut
+    AfficherPage($routes["default"]);
+
+}
