9083519043949e29e827c167b9895b25e32db1c2
nilsKolliker <nils.kolliker@gmail.com>
2021-04-27 11:49:49 +0200

    formulaire recette

--- PHP/VIEW/Head.php
+++ PHP/VIEW/Head.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/style.css"></linkrel>
     <title><?= $titre;?></title>
+    <script src="https://kit.fontawesome.com/a6b48dfea6.js" crossorigin="anonymous"></script>
 </head>
 <body>
